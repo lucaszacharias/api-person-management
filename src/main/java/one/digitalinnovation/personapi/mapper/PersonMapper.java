29def6a4a47f4716843c3e53dadd7b2fecd2e2ce
lucaszacharias <lucaszacharias@hotmail.com>
2021-10-01 17:18:13 -0300

    Nor Working

--- src/main/java/one/digitalinnovation/personapi/mapper/PersonMapper.java
+++ src/main/java/one/digitalinnovation/personapi/mapper/PersonMapper.java
@@ -6,12 +6,12 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
-@Mapper
+@Mapper(componentModel = "spring")
 public interface PersonMapper {
 
     PersonMapper INSTANCE = Mappers.getMapper(PersonMapper.class);
 
-    @Mapping(target = "personDTO.birthDate", source = "birthDate")
+    @Mapping(target = "birthDate", source = "birthDate")
     Person toModel(PersonDTO personDTO);
 
     PersonDTO toDTO(Person person);
